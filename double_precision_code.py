5d3935b833ce785eaa211455c8c15162283b2443
mestakevin <kmmesta@syr.edu>
2024-09-24 13:09:23 -0400

    started class of double preci and main function

---
+++ double_precision_code.py
@@ -0,0 +1,38 @@
+
+
+class doublePrec():
+
+    def __init__(self, mantissa, exponent, sign):
+        self.mantissa = mantissa
+        self.exponent = exponent
+        self.sign = sign
+
+
+    def getMantissa(self):
+        return self.mantissa
+
+    
+    def getExponent(self):
+        return self.exponent
+
+
+    def getSign(self):
+        return self.sign
+
+    def __mul__(self, other):
+        new_man = self.mantissa * other.mantissa
+        new_exp = self.exponent + other.exponent
+        new_sign = self.sign * other.sign
+        return new_man, new_exp, new_sign
+
+def convertDouble(num):
+        
+
+def main():
+        num = doublePrec(1.2,10,-1)
+        num2 = doublePrec(6.0, 0.1, -1)
+        print
+   
+
+
+main()
