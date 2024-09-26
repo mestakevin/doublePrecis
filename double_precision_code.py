20a10fb1ada48e2fa55e07eaad536fd1ef2b5ebe
Sara-Talebi <stalebi@g.syr.edu>
2024-09-26 11:30:10 -0400

    Update double_precision_code.py
    adding the function of showing scientific notation

--- double_precision_code.py
+++ double_precision_code.py
@@ -23,37 +23,33 @@ class doublePrec():
         new_man = self.mantissa * other.mantissa
         new_exp = self.exponent + other.exponent
         new_sign = self.sign * other.sign
-        return new_man, new_exp, new_sign
+        return doublePrec(new_man, new_exp, new_sign)
     
     def __truediv__(self, other):
         new_man = self.mantissa / other.mantissa
         new_exp = self.exponent - other.exponent
         new_sign = self.sign * other.sign
-        return new_man, new_exp, new_sign
+        return doublePrec(new_man, new_exp, new_sign)
 
     def __add__(self, other):
-        if self.exponent != other.exponent:
-            diff_exp = self.exponent - other.exponent
-            new_exp  = other.exponent + diff_exp
-            mew_man = other.mantissa * (10** -diff_exp)
-            new_man = other.sign * mew_man + self.sign * self.mantissa
-            if new_man < 0:
-                new_sign = -1
+            # Align exponents
+            if self.exponent > other.exponent:
+                diff_exp = self.exponent - other.exponent
+                adjusted_other_man = other.mantissa * (10 ** diff_exp)
+                new_man = self.sign * self.mantissa + other.sign * adjusted_other_man
+                new_exp = self.exponent
             else:
-                new_sign = 1
-            return new_man, new_exp, new_sign
+                diff_exp = other.exponent - self.exponent
+                adjusted_self_man = self.mantissa * (10 ** diff_exp)
+                new_man = self.sign * adjusted_self_man + other.sign * other.mantissa
+                new_exp = other.exponent
+    
+            new_sign = -1 if new_man < 0 else 1
+            return doublePrec(abs(new_man), new_exp, new_sign)
 
-        else:
-            new_man = self.sign * self.mantissa + other.sign * other.mantissa
-            new_exp = self.exponent
-            if new_man < 0:
-                new_sign = -1
-            else:
-                new_sign = 1
-            return new_man, new_exp, new_sign    
-
-
-        
+    def __str__(self):
+        # Display number in scientific notation
+        return f"{self.sign * self.mantissa}e{self.exponent}"    
 
 
 def convertDouble():
@@ -61,9 +57,9 @@ def convertDouble():
     b = -6.0e7
     c = a * b
     d = a/b
-    print("Alg")
-    print("mult: ", c)
-    print("div: ", d)
+    print("Standard Python:")
+    print("Mult:", c)
+    print("Div:", d)
     
         
 
@@ -71,7 +67,7 @@ def main():
         num = doublePrec(1.2,10,-1)
         num2 = doublePrec(6.0, 7, -1)
         num3 = num * num2
-        print("our number: ", num3)
+        print("Our number (custom class):", num3)
         
         convertDouble()
    
