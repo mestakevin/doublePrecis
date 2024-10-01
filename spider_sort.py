5b099f315099821c381a9a4860127d0eaac1664a
mestakevin <kmmesta@syr.edu>
2024-10-01 13:28:13 -0400

    implemented graph

--- spider_sort.py
+++ spider_sort.py
@@ -5,14 +5,16 @@ from matplotlib import pyplot as plt
 def bubbleSort(a_list):
     length = len(a_list)
     temp_list = []
+    time_now = time.time()
     for i in range(0, length-1):
         for j in i:
             if a_list[i] > a_list[i+1]
                 temp = a_list[i]
                 a_list[i] = a_list[i+1]
                 a_list[i+1] = temp
-
-    return a_list
+    time_end = time.time()
+    tot_time = time_end - time_now
+    return a_list, tot_time
             
 
 # Function to generate a list of random numbers
@@ -40,6 +42,25 @@ def main():
     plt.title('List size as a Function of Time')
     plt.grid()
 
-    plt.show()
+    n = int(1e3)
+    N = 1000 #final  
+    lower_bound = 0
+    upper_bound = 100
+    random_list = generate_random_list(n, lower_bound, upper_bound)
+    print(random_list)
+    time_list=[]
 
+    for i in range(1,N,1):
+        cur_list = generate_random_list(i, 0, 100)
+        sorted_list, tot_time = bubbleSort(cur_list)
+        time_list.append(tot_time)
+
+    #plotting
+    plt.plot(range(1,N,1), time_list)
+    plt.xlabel('List Size')
+    plt.ylabel('List Values')
+    plt.title('Lists as A Function of List Size')
+    plt.grid()
+
+    plt.show()
 main()
