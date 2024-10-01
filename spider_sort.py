fb235c469b939346ff1d2681480806d6ed1df571
cacapuano <cecapuan@syr.edu>
2024-10-01 13:14:29 -0400

    Update spider_sort.py

--- spider_sort.py
+++ spider_sort.py
@@ -25,21 +25,21 @@ def generate_random_list(n, lower_bound, upper_bound):
 
 def main():
   n = int(1e3)
-  N = 1000 #final  
+  N = 10 #final  
   lower_bound = 0
   upper_bound = 100
   random_list = generate_random_list(n, lower_bound, upper_bound)
   print(random_list)
-  for i in range (len(random_list), N):
-      
-  return list_size
+  for i in range (0, N):
+      length = len(random_list)
+  return length
 #plotting
-plt.plot(time,list_size)
-plt.xlabel('List Size')
-plt.ylabel('List Values')
-plt.title('Lists as A Function of List Size')
-plt.grid()
+    plt.plot(time,length)
+    plt.xlabel('Time')
+    plt.ylabel('List size')
+    plt.title('List size as a Function of Time')
+    plt.grid()
 
-plt.show()
+    plt.show()
 
 main()
