739afacf03cbf31f646d9bbe06fde68a33fc3970
mestakevin <kmmesta@syr.edu>
2024-10-01 13:01:27 -0400

    added bubble sort method`

--- spider_sort.py
+++ spider_sort.py
@@ -1,2 +1,28 @@
+
+
+
+def bubbleSort(a_list):
+    length = len(a_list)
+    temp_list = []
+    for i in range(0, length-1):
+        for j in i:
+            if a_list[i] > a_list[i+1]
+                temp = a_list[i]
+                a_list[i] = a_list[i+1]
+                a_list[i+1] = temp
+
+    return a_list
+            
+
+
+
+
+
 def main():
-  
+
+
+
+
+
+
+
