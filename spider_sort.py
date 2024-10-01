2f0bc70a15bece78d0f2b964a357d2970fe0b15d
Sara-Talebi <stalebi@g.syr.edu>
2024-10-01 12:57:26 -0400

    random int# generator func with lower/upper bound

--- spider_sort.py
+++ spider_sort.py
@@ -1,14 +1,18 @@
 import random
 
 # Function to generate a list of random numbers
-def generate_random_list(n):
+def generate_random_list(n, lower_bound, upper_bound):
     random_list = []
     for i in range(n):
-        random_number = random.random()  
+        random_number = random.randint(lower_bound, upper_bound)  
         random_list.append(random_number)
     return random_list
 
 def main():
-  n = 1e3
-  random_list = generate_random_list(n)
+  n = int(1e3)
+  lower_bound = 0
+  upper_bound = 100
+  random_list = generate_random_list(n, lower_bound, upper_bound)
   print(random_list)
+  
+main()
