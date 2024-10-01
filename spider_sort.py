5c4603518caa042685e887455463c6ba53c8c5e6
Sara-Talebi <stalebi@g.syr.edu>
2024-10-01 12:51:29 -0400

    Update spider_sort.py
    generate random list size n function added

--- spider_sort.py
+++ spider_sort.py
@@ -1,2 +1,14 @@
+import random
+
+# Function to generate a list of random numbers
+def generate_random_list(n):
+    random_list = []
+    for i in range(n):
+        random_number = random.random()  
+        random_list.append(random_number)
+    return random_list
+
 def main():
-  
+  n = 1e3
+  random_list = generate_random_list(n)
+  print(random_list)
