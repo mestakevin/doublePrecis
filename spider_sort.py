263119223a079e37b6dd6708c7336e6837445b11
cacapuano <cecapuan@syr.edu>
2024-10-02 12:54:15 -0400

    Update spider_sort.py
    missing : in line 11

--- spider_sort.py
+++ spider_sort.py
@@ -8,7 +8,7 @@ def bubbleSort(a_list):
     time_now = time.time()
     for i in range(0, length-1):
         for j in i:
-            if a_list[i] > a_list[i+1]
+            if a_list[i] > a_list[i+1]:
                 temp = a_list[i]
                 a_list[i] = a_list[i+1]
                 a_list[i+1] = temp
