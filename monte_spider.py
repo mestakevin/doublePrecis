962388b329a3f132486ac03258eb00db1db5f493
cacapuano <cecapuan@syr.edu>
2024-10-03 13:22:21 -0400

    Update monte_spider.py

--- monte_spider.py
+++ monte_spider.py
@@ -3,7 +3,7 @@
 # Function to generate a list of random numbers
 def generate_random_list(n, lower_bound, upper_bound):
     random_list = []
-    for i in range(0,1):
+    for i in range(n):
         random_number = random.uniform(lower_bound, upper_bound)  
         random_list.append(random_number)
     return random_list
