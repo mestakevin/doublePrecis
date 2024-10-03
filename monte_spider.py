d7efe7139f326673247614becd21ccc4dfa739ae
cacapuano <cecapuan@syr.edu>
2024-10-03 13:21:48 -0400

    Update monte_spider.py

--- monte_spider.py
+++ monte_spider.py
@@ -1,3 +1,10 @@
 #Monte Carlo assignment
 
-#integrate probability
+# Function to generate a list of random numbers
+def generate_random_list(n, lower_bound, upper_bound):
+    random_list = []
+    for i in range(0,1):
+        random_number = random.uniform(lower_bound, upper_bound)  
+        random_list.append(random_number)
+    return random_list
+
