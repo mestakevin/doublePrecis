303d65a8044e7d9882533ee45776b35235114708
Sara-Talebi <stalebi@g.syr.edu>
2024-10-03 13:32:18 -0400

    Update ellipse_spider.py
    ellipse plotting

--- ellipse_spider.py
+++ ellipse_spider.py
@@ -1,1 +1,16 @@
 #Ellipse circumference 
+import numpy as np
+from matplotlib import pyplot as plt
+from math import pi
+def main():
+
+
+  u=1.     #x-position of the center
+  v=0.5    #y-position of the center
+  a=2.     #radius on the x-axis
+  b=1.5    #radius on the y-axis
+  
+  t = np.linspace(0, 2*pi, 100)
+  plt.plot( u+a*np.cos(t) , v+b*np.sin(t) )
+  plt.grid(color='lightgray',linestyle='--')
+  plt.show()
