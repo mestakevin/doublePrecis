30b5503d48c4b0adc74ef08d1787000dac583263
cacapuano <cecapuan@syr.edu>
2024-10-08 13:14:46 -0400

    Update line_prab_spider.py
    line with a function of y=1.71x +1.01

--- line_prab_spider.py
+++ line_prab_spider.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 
 def draw_rand_points(num):
     x_rand_array = np.random.uniform(low=0.0, high=1.0, size=num)
-    upper_bound = math.e + 0.1
+    upper_bound = math.e + 0.01 #upper bound of function
     y_rand_array = np.random.uniform(low=0.0, high=upper_bound, size=num)
     return x_rand_array, y_rand_array
 
@@ -21,7 +21,7 @@ def main():
     for i in range(0,len(rand_x)-1,1):
         x_val = rand_x[i]
         y_val = rand_y[i]
-        if (y_val <= (math.e ** x_val) ):
+        if (y_val <= (1.71*num_points + 1.01) ):
             kept_xs.append(x_val)
             kept_ys.append(y_val)
 
