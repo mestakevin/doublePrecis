e5d89617ffeff493f21c737ab75c5cc25a5da370
Sara-Talebi <stalebi@g.syr.edu>
2024-10-08 13:17:02 -0400

    adding the proposal func and line part

--- line_prab_spider.py
+++ line_prab_spider.py
@@ -1,10 +1,12 @@
 #proposal function of uniform, line, and prabola.
-
-## rejection method for e^x
-
 import numpy as np
 import math
 import matplotlib.pyplot as plt
+
+def proposal_func(x):
+    #for line
+    y = (1.71 * x) + 1.01
+    return y
 
 def draw_rand_points(num):
     x_rand_array = np.random.uniform(low=0.0, high=1.0, size=num)
@@ -14,14 +16,15 @@ def draw_rand_points(num):
 
 
 def main():
-    num_points = 10000
+    num_points = int(1e6)
     rand_x, rand_y = draw_rand_points(num_points)
     kept_xs = []
     kept_ys = []
     for i in range(0,len(rand_x)-1,1):
         x_val = rand_x[i]
         y_val = rand_y[i]
-        if (y_val <= (math.e ** x_val) ):
+        y_propos_func = proposal_func(x_val)
+        if y_val <= y_propos_func:
             kept_xs.append(x_val)
             kept_ys.append(y_val)
 
