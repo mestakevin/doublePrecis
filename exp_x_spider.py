08700bbfb9cc9f2c942c004df96d5eb8482ef91d
Sara-Talebi <stalebi@g.syr.edu>
2024-10-10 13:06:08 -0400

    creation of the file

---
+++ exp_x_spider.py
@@ -0,0 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy as sp
+
+##-----------------------------------------------------------------##
+def func(x):
+    y = e**x
+    return y 
+##-----------------------------------------------------------------##
+def main():
+ 	
+ 	
+ 	
+    plt.figure()
+    plt.plot(t,x1, label='')
+    plt.plot(t,x2, label='')
+    plt.xlabel('')
+    plt.ylabel('')
+    plt.title('')
+    plt.legend()
+    plt.show()
+##-----------------------------------------------------------------##
+main()
+##-----------------------------------------------------------------##
+ 
