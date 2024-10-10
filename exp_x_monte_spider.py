3ea6c75a18658e503391525971c9063a42364025
Sara-Talebi <stalebi@g.syr.edu>
2024-10-10 13:23:23 -0400

    adding numerical integration to the file

--- exp_x_monte_spider.py
+++ exp_x_monte_spider.py
@@ -4,22 +4,35 @@ import math
 import scipy as sp
 
 ##-----------------------------------------------------------------##
+def trapz_rule_v1(n, up_lim, low_lim, func):
+    delta_x = (up_lim - low_lim) / n
+    k_sum = 0.0
+    for i in range(1, n):
+        temp_val = func(low_lim + (i * delta_x))
+        k_sum += temp_val
+    temp_val2 = (func(up_lim) + func(low_lim)) / 2
+    final_val = delta_x * (temp_val2 + k_sum)
+    return final_val
+##-----------------------------------------------------------------##
 def func(x):
-    y = e**x
-    return y 
-
-
+    y = (math.e)**x
+    return y
+##-----------------------------------------------------------------##
 def draw_rand_points(num):
     x_rand_array = np.random.uniform(low=0.0, high=1.0, size=num)
     y_rand_array = np.random.uniform(low=0.0, high=math.e, size=num)
 
     return x_rand_array, y_rand_array
-
-
-
-
 ##-----------------------------------------------------------------##
 def main():
+    # Set up integration limits and parameters
+    up_lim = 1  # upper limit
+    low_lim = 0  # lower_limit
+    n = int(1e3)  # number of divisions for integration
+    
+    # Compute the integral using Trapezoidal rule
+    value = trapz_rule_v1(n, up_lim, low_lim, func)
+    print(f"Trapezoidal= \t {value}")    
     area =  math.e
     hits = 0
     
