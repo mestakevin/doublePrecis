28611c744bcfb45f846f0ca23e3ecd6d36e6f67a
mestakevin <kmmesta@syr.edu>
2024-10-17 13:11:16 -0400

    added small message

--- exp_x_monte_spider.py
+++ exp_x_monte_spider.py
@@ -22,27 +22,46 @@ def draw_rand_points(num):
     x_rand_array = np.random.uniform(low=0.0, high=1.0, size=num)
 
     return x_rand_array
+
+def choice_func(x):
+    y = x**2 + x + 1
+    return y
+
 ##-----------------------------------------------------------------##
 def main():
     # Set up integration limits and parameters
     up_lim = 1  # upper limit
     low_lim = 0  # lower_limit
-    n = int(1e3)  # number of divisions for integration
+    n = int(1e6)  # number of divisions for integration
     
     # Compute the integral using Trapezoidal rule
     value = trapz_rule_v1(n, up_lim, low_lim, func)
     print(f"Trapezoidal= \t {value}")    
-    area_true =  math.e
+    area_true =  math.e -1
     hits = 0
     area_int = 0
     x_rand_array = draw_rand_points(n)
 
     for i,value in enumerate(x_rand_array):
         area_int += math.e ** value * 1/n
-
+        
     print(area_int)
     print(math.e - 1)
  	
+    sum_of_px = 0
+    for i, value in enumerate(x_rand_array):
+        p_x = choice_func(value)
+        sum_of_px += 1/p_x
+
+    c = 1/sum_of_px
+    
+    choice_func_integral = 0
+    for i, value in enumerate(x_rand_array):
+        mini_chunk = func(value) * c/(choice_func(value))
+        choice_func_integral += mini_chunk
+
+    print(choice_func_integral)
+
     plt.figure()
     plt.xlabel('')
     plt.ylabel('')
@@ -52,4 +71,4 @@ def main():
 ##-----------------------------------------------------------------##
 main()
 ##-----------------------------------------------------------------##
- 
+#small change 
