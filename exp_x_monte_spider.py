660b961e5450dbbd68c0e10b1eb4438bd510e70f
mestakevin <kmmesta@syr.edu>
2024-10-17 13:42:58 -0400

    changes for code

--- exp_x_monte_spider.py
+++ exp_x_monte_spider.py
@@ -19,7 +19,7 @@ def func(x):
     return y
 ##-----------------------------------------------------------------##
 def draw_rand_points(num):
-    x_rand_array = np.random.uniform(low=0.0, high=1.0, size=num)
+    x_rand_array = np.random.uniform(low=0.0, high=math.pi, size=num)
 
     return x_rand_array
 
@@ -32,40 +32,42 @@ def fav_function(x):
     return sine_fun
 
 def fav_choice_fun(x):
-    y_fav = -0.4 * x**2 + 1.1 * x + 0.6
+    #y_fav = np.sin(x)
+    y_fav = -0.4 * x**2 + 1.2 * x + 0.2
     return y_fav
 
 ##-----------------------------------------------------------------##
 def main():
     # Set up integration limits and parameters
-    up_lim = 1  # upper limit
+    up_lim = math.pi  # upper limit
     low_lim = 0  # lower_limit
     n = int(1e6)  # number of divisions for integration
     
     # Compute the integral using Trapezoidal rule
-    value = trapz_rule_v1(n, up_lim, low_lim, func)
+    value = trapz_rule_v1(n, up_lim, low_lim, fav_function)
     print(f"Trapezoidal= \t {value}")    
-    area_true =  math.e -1
+    area_true =  2
     hits = 0
     area_int = 0
     x_rand_array = draw_rand_points(n)
-
+    domain = up_lim - low_lim
+    print(domain)
     for i,value in enumerate(x_rand_array):
-        area_int += math.e ** value * 1/n
+        area_int += math.sin(value) * domain/n
         
     print(area_int)
-    print(math.e - 1)
+    print(2)
  	
     sum_of_px = 0
     for i, value in enumerate(x_rand_array):
-        p_x = choice_func(value)
-        sum_of_px += 1/p_x
+        p_x = fav_choice_fun(value)
+        sum_of_px += domain/p_x
 
-    c = 1/sum_of_px
-    
+    c = domain/sum_of_px
+    print(c)
     choice_func_integral = 0
     for i, value in enumerate(x_rand_array):
-        mini_chunk = func(value) /(choice_func(value))
+        mini_chunk = fav_function(value) /(fav_choice_fun(value))
         choice_func_integral += mini_chunk
 
     print(choice_func_integral*c)
