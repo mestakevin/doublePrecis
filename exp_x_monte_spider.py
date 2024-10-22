cc590f6c47b984aac4910ca1041e334aa66e32f4
mestakevin <kmmesta@syr.edu>
2024-10-22 13:08:10 -0400

    ediedted monte spider

--- exp_x_monte_spider.py
+++ exp_x_monte_spider.py
@@ -19,8 +19,7 @@ def func(x):
     return y
 ##-----------------------------------------------------------------##
 def draw_rand_points(num):
-    x_rand_array = np.random.uniform(low=0.0, high=math.pi, size=num)
-
+    x_rand_array = np.random.uniform(low=0.0, high=math.e, size=num)
     return x_rand_array
 
 def choice_func(x):
@@ -33,51 +32,65 @@ def fav_function(x):
 
 def fav_choice_fun(x):
     #y_fav = np.sin(x)
-    y_fav = -0.4 * x**2 + 1.2 * x + 0.2
+    y_fav = (-0.4) * ( x**2) + (1.2 * x) + 0.4
     return y_fav
 
 ##-----------------------------------------------------------------##
 def main():
     # Set up integration limits and parameters
-    up_lim = math.pi  # upper limit
+    #up_lim = math.pi  # upper limit
+    up_lim = 1
+
     low_lim = 0  # lower_limit
     n = int(1e6)  # number of divisions for integration
     
     # Compute the integral using Trapezoidal rule
-    value = trapz_rule_v1(n, up_lim, low_lim, fav_function)
+    #value = trapz_rule_v1(n, up_lim, low_lim, fav_function) 
+    value = trapz_rule_v1(n, up_lim, low_lim, func)
+
     print(f"Trapezoidal= \t {value}")    
-    area_true =  2
+    
+    #area_true =  2
+    area_true = math.e - 1
+    
     hits = 0
     area_int = 0
     x_rand_array = draw_rand_points(n)
     domain = up_lim - low_lim
     print(domain)
+    
     for i,value in enumerate(x_rand_array):
-        area_int += math.sin(value) * domain/n
+        area_int += math.exp(value) * domain/n
+        #area_int += math.sin(value) * domain/n
         
     print(area_int)
-    print(2)
- 	
+    #print(2)
+    print(area_true)
+    
     sum_of_px = 0
     for i, value in enumerate(x_rand_array):
-        p_x = fav_choice_fun(value)
+       # p_x = fav_choice_fun(value)
+        p_x = choice_func(value)
+        
         sum_of_px += domain/p_x
 
     c = domain/sum_of_px
     print(c)
+    
     choice_func_integral = 0
     for i, value in enumerate(x_rand_array):
-        mini_chunk = fav_function(value) /(fav_choice_fun(value))
+        #mini_chunk = fav_function(value) /(fav_choice_fun(value))
+        mini_chunk = func(value) /(choice_func(value))
         choice_func_integral += mini_chunk
 
     print(choice_func_integral*c)
 
-    plt.figure()
-    plt.xlabel('')
-    plt.ylabel('')
-    plt.title('')
-    plt.legend()
-    plt.show()
+    #plt.figure()
+    #plt.xlabel('')
+    #plt.ylabel('')
+    #plt.title('')
+    #plt.legend()
+    #plt.show()
 ##-----------------------------------------------------------------##
 main()
 ##-----------------------------------------------------------------##
